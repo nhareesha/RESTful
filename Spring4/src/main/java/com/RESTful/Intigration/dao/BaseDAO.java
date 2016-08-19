36f778f9606ca8c1e30ae80dc239c2682cd15863
hareesha <hareesha1906@gmail.com>
2016-08-19 11:51:36 -0700

    Transaction management

--- Spring4/src/main/java/com/RESTful/Intigration/dao/BaseDAO.java
+++ Spring4/src/main/java/com/RESTful/Intigration/dao/BaseDAO.java
@@ -1,8 +1,10 @@
 package com.RESTful.Intigration.dao;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.RESTful.Intigration.beans.EnquiryResponse;
 
 public interface BaseDAO {
+	@Transactional
 	public void insertRecord(EnquiryResponse e);
-
 }
